6e3d0eef3cb89f423c4b8bc4a60970f5a0086359
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2023-06-21 12:30:39 -0300

    Refactor and add types

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import fs from "node:fs/promises";
 import parseTemplate from "./lib/parse-template.js";
+import * as Types from "./lib/types/types.js";
 
 const render = parseTemplate/*html*/ `<!DOCTYPE html>
 <html lang="en">
@@ -9,24 +10,24 @@ const render = parseTemplate/*html*/ `<!DOCTYPE html>
     <title>${"pageTitle"}</title>
   </head>
   <body>
-    <!-- each expression must be inside an element and separate lines -->
-    <h1>${"pageTitle"}</h1>
-    <p>${"firstName"}</p>
-    <p>${"hobbies"}</p>
+    <h1>Current page: ${"pageTitle"}</h1>
+    <p>Name: ${"firstName"}</p>
+    <p>Ocupation ${"ocupation"}</p>
+    <!-- <p>This comment will be ignored</p> -->
+    <h2>My hobbies</h2>
+    <ul>
+      <li>${"hobbies"}</li>
+    </ul>
   </body>
 </html>
 `;
 
-/**
- * Type definition based on template expressions
- *
- * @typedef{{ pageTitle:string, firstName: string, hobbies: string[] }} ViewData
- */
-
-/** @type ViewData */
+/** @type {Types.ViewData} */
 const viewData = {
   pageTitle: "Tagged Templates - Home",
   firstName: "Pedro",
+  ocupation: "worker",
+  alive: true,
   hobbies: ["movies", "games"],
 };
 
